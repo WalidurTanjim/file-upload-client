1b767dfab0d4cdd094688096a79376e7da792080
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-11-27 20:26:49 +0600

    User info with image uploaded to mongodb successfully with image live link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import UploadedImage from '../../components/UploadedImage/UploadedImage';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Home = () => {
     const [file, setFile] = useState('');
@@ -41,8 +42,28 @@ const Home = () => {
                 image: image
             })
             console.log(result.data);
-            const image_public_id = result?.data.public_id;
-            setImagePublicId(image_public_id);
+            if(result?.data){
+                // image public_id
+                const image_public_id = result?.data.public_id;
+                setImagePublicId(image_public_id);
+
+                // image secure_url (live link)
+                const image_secure_url = result?.data?.secure_url;
+                const user = { username, email, password, image_secure_url };
+                console.log(user);
+                try{
+                    const res = await axios.post('http://localhost:5000/user', user)
+                    const data = await res?.data;
+                    console.log(data);
+
+                    if(data?.insertedId){
+                        toast.success('Successfully toasted!');
+                    }
+
+                }catch(err){
+                    console.error(err);
+                }
+            }
         } catch (err) {
             console.error(err);
         }
@@ -54,17 +75,17 @@ const Home = () => {
                 <form className='w-full mx-auto md:w-[550px]' onSubmit={handleSubmit}>
                     <div className='username mb-3'>
                         <label htmlFor="username" className='text-sm text-slate-700 font-medium'>Full Name</label>
-                        <input type="text" name="username" id="username" className="file-input file-input-bordered w-full px-2"  />
+                        <input type="text" name="username" id="username" className="file-input file-input-bordered w-full px-2" required />
                     </div>
 
                     <div className='email mb-3'>
                         <label htmlFor="email" className='text-sm text-slate-700 font-medium'>Email Address</label>
-                        <input type="email" name="email" id="email" className="file-input file-input-bordered w-full px-2"  />
+                        <input type="email" name="email" id="email" className="file-input file-input-bordered w-full px-2" required />
                     </div>
 
                     <div className='password mb-3'>
                         <label htmlFor="password" className='text-sm text-slate-700 font-medium'>Password</label>
-                        <input type="text" name="password" id="password" className="file-input file-input-bordered w-full px-2"  />
+                        <input type="text" name="password" id="password" className="file-input file-input-bordered w-full px-2" required />
                     </div>
 
                     <div className='photo mb-3'>
@@ -76,13 +97,17 @@ const Home = () => {
                 </form>
 
                 <div className='mt-10 w-full md:w-[550px] h-[300px] mx-auto rounded-xl border p-3'>
+                    <h1 className='text-sm text-slate-700 font-medium mb-2'>Selected Image:</h1>
                     <img src={image} alt="No image available" className='w-full h-full rounded-md' />
                 </div>
 
                 <div className='mt-10 w-full md:w-[550px] h-[300px] mx-auto rounded-xl border p-3'>
+                    <h1 className='text-sm text-slate-700 font-medium mb-2'>Uploaded Image:</h1>
                     <UploadedImage imagePublicId={imagePublicId} />
                 </div>
             </div>
+
+            <Toaster />
         </div>
     );
 };
